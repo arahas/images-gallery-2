3655c66e69dca90fe18f429d5ebe0bf0eeecaddf
Jinwoo Je <jeff.jw.je@gmail.com>
2024-01-21 09:16:41 -0800

    wip for useState for word in form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
+  console.log(word);
+
   return (
     <div>
-      <Header title="Images Gallery 2"/>
+      <Header title="Images Gallery 2" />
+      <Search word = {word} setWord = {setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
